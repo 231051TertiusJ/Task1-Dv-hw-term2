76bfb336a342076100c8ae1fe9fb85e8efa4cd51
231051TertiusJ <125471734+231051TertiusJ@users.noreply.github.com>
2023-05-12 13:15:36 +0200

    work on

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var firstName = "Tertius";
+var surname = "Jonker";
+let age = 18;
+const subject = "Development";
+
+let message = "Hello my name is " + firstName + " " + surname + ", I am " + age + " years old and I am studying " + subject;
+
+console.log(message)
+
+let numOne = 22;
+let numTwo = 33;
+let numThree = 44;
+let numFour = 55;
+let answer = 0;
+
+let total = numOne + numTwo / numThree * numFour;
+
+console.log(total)
+
+let timmy = 10;
+let susan = 25;
+let timTax = 15/100;
+let susTax = 26/100;
+
+let value = timmy * timTax + susan * susTax;
+
+console.log(value)
